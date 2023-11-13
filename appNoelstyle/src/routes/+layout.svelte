49324b36de84f8a1be64f4401d959ac0ef55daad
astridelepine <delas2924@gmail.com>
2023-11-13 22:10:51 +0100

    ok tout marche

--- appNoelstyle/src/routes/+layout.svelte
+++ appNoelstyle/src/routes/+layout.svelte
@@ -2,99 +2,76 @@
 	//import './styles.css';
 	import '../style/app.css';
 	import { writable, derived } from 'svelte/store';
-
+	import {tick} from 'svelte';
 	
 	let repprenom = null;
 	let repmdp = '';
 	let repbenf = null;
-	let reppp = null;
-	
-	const prenombd = () => {
-		let error = null;
-		
-
-		fetch(`http://127.0.0.1:8000/prenom/${prenom}`, {
-		method: 'GET',
-		headers: {
-			'accept': 'application/json'
-		}
-		})
-		.then(response => response.json())
-		.then(data => {
-			repprenom = data;
-		})
-		.catch(err => {
-			error = err;
-		})
-		
-	};
-
-	
-	const mdpbd = () => {
-		let error = null;
-
-		fetch(`http://127.0.0.1:8000/mdp/${prenom}`, {
-		method: 'GET',
-		headers: {
-			'accept': 'application/json'
-		}
-		})
-		.then(response => response.json())
-		.then(data => {
-			repmdp = data;
-		})
-		.catch(err => {
-			error = err;
-		})
-
-		
-	};
-
-	const recupbeneficiaire = () => {
-		let error = null;
-
-		fetch(`http://127.0.0.1:8000/beneficiaire/${prenom}`, {
-		method: 'GET',
-		headers: {
-			'accept': 'application/json'
-		}
-		})
-		.then(response => response.json())
-		.then(data => {
-			reppp= data;
-		})
-		.then(()=> {
-			if(mdp === repmdp){
-				codebon = true
-			}
-			else{
-				codebon = false
-			}
-
-			if(repprenom!= 'null' && repmdp == mdp){
-				afficher = true
-			}
-		})
-		.catch(err => {
-			error = err;
-		})
-
-		
-		
-	};
-
 	
 	let codebon;
 	let afficher = false;
 	let prenom = '';
 	let mdp = '';
-	$:console.log(prenom ==='' && mdp ==='');
-	$:console.log(prenom)
+
+	// $:console.log(prenom ==='' && mdp ==='');
+	// $:console.log(prenom)
 
 	
-	$:console.log(prenom ==='' && mdp ==='');
-	$:console.log(prenombd);
-	$:console.log(repmdp);
+	// $:console.log(prenom ==='' && mdp ==='');
+	// $:console.log(prenombd);
+	// $:console.log(repmdp);
+	
+	const prenombd = async() => {
+		let error = null;
+		let response = await fetch(`http://127.0.0.1:8000/prenom/${prenom}`);
+
+		response = await response.json();
+		repprenom = await response;
+		$:console.log(repprenom);
+		
+	};
+
+	
+	const mdpbd = async() => {
+		let error = null;
+		let response = await fetch(`http://127.0.0.1:8000/mdp/${prenom}`);
+		
+		response = await response.json();
+		repmdp = response;
+		
+	};
+
+	const recupbeneficiaire = async() => {
+		let error = null;
+		let response = await fetch(`http://127.0.0.1:8000/beneficiaire/${prenom}`);
+
+		response = await response.json();
+		repbenf = response;
+		
+	};
+
+	const traitementbouton = async (e) => {
+			e.preventDefault();
+			await prenombd();
+			await mdpbd();
+			await recupbeneficiaire();
+			
+			if(mdp == repmdp){	
+				codebon = true;
+				$:console.log(codebon);
+			}
+			else{
+				codebon = false;
+			}
+
+
+			if(repprenom!= 'null' && repmdp == mdp){
+				afficher = true;
+				
+			}
+			
+		};
+	
 
 </script>
 
@@ -108,7 +85,7 @@
 		{#if afficher == false}
 			<div id='id' class=" flex flex-col items-center rounded-xl border-black ml-3 mt-4 px-7 py-4 shadow-md">
 				<h1 class = "flex center"> <img class="object-cover h-7 w-7" src="https://www.gifimili.com/gif/2018/05/smiley-guirlande-de-noel.gif"  alt="Smiley Guirlande de Noël"> Qui es-tu ? <img class="object-cover h-7 w-7" src="https://www.gifimili.com/gif/2018/05/smiley-guirlande-de-noel.gif"  alt="Smiley Guirlande de Noël"></h1>
-				<form on:submit class = "flex flex-col items-center">
+				<form on:submit={traitementbouton} class = "flex flex-col items-center">
 					<label class ="mb-2 mt-3 flex"> Ton nom :
 						<input bind:value={ prenom } class = "border border-1 rounded border-black ml-3 bg" type= 'text' name="prenom" maxlength="55" size="7" />
 					</label>
@@ -116,11 +93,11 @@
 					<label class ="mb-5"> Ton code :
 						<input bind:value={ mdp } class = "border border-1 rounded border-black ml-3" type= 'password' name="code" maxlength="55" size="7" />
 					</label>
-					<button on:click = {mdpbd} on:click = {prenombd} on:click = {recupbeneficiaire} disabled={prenom==='' || mdp===''} type="submit" class = " text-center border border-1 rounded border-black w-fit px-2 flex " > Voir </button>
+					<button disabled={prenom==='' || mdp===''} type="submit" class = " text-center border border-1 rounded border-black w-fit px-2 flex " > Voir </button>
 				</form>
 
 				{#if repprenom === 'null'}
-					<p> <br/> je crois que tu ne fais pas partie de ce secret Santa {repprenom} {repbenf} </p>
+					<p> <br/> je crois que tu ne fais pas partie de ce secret Santa  </p>
 				{:else if codebon == false}
 					<p> <br/> hopopop mauvais code... </p>
 				{:else}
@@ -132,7 +109,8 @@
 			<div id='id' class=" flex flex-col items-center rounded-xl border-black ml-3 mt-4 px-7 py-4 shadow-md">
 				<h1 class = "flex center"> <img class="object-cover h-7 w-7" src="https://www.gifimili.com/gif/2018/05/smiley-guirlande-de-noel.gif"  alt="Smiley Guirlande de Noël"> Qui es-tu ? <img class="object-cover h-7 w-7" src="https://www.gifimili.com/gif/2018/05/smiley-guirlande-de-noel.gif"  alt="Smiley Guirlande de Noël"></h1>
 				<p> <br/> Tu as pour mission d'offrir </p>
-				<p> un cadeau a {reppp} !!! </br> </p>
+				<p class = "mb-3"> un cadeau a {repbenf} !!! <br/> </p>
+				
 				<button on:click = {() => {afficher = false}} type="submit" class = " text-center border border-1 rounded border-black w-fit px-2 flex " > Retour </button>
 			</div>
 		
--- appNoelstyle/svelte.config.js
+++ appNoelstyle/svelte.config.js
@@ -9,10 +9,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		vite: {
-			// Spécifiez le préprocesseur Svelte pour TypeScript
-			plugins: [vitePreprocess()],
-		},
+	
 	},
 	preprocess: vitePreprocess(),
 };
